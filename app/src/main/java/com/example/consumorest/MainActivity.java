f7956647fdc5f1c5741bcb69ab706c4d39343ce1
Bryan Zúñiga <bryan.zuniga@udla.edu.ec>
2019-11-06 02:36:55 -0500

    consumo json

--- app/src/main/java/com/example/consumorest/MainActivity.java
+++ app/src/main/java/com/example/consumorest/MainActivity.java
@@ -3,12 +3,104 @@ package com.example.consumorest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Base64;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+    private ArrayAdapter arrayAdapter;
+    private ArrayList<String> titles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, titles);
+        list = findViewById(R.id.list);
+
+        list.setAdapter(arrayAdapter);
+
+        consumirRest();
+    }
+
+    private void consumirRest() {
+
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        final StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()) {
+                    try {
+
+                        Gson gson = new Gson();
+                        List<Post> posts = gson.fromJson(response, new TypeToken<List<Post>>() {
+                        }.getType());
+
+                        for (Post post : posts) {
+                            titles.add(post.getTitle());
+                        }
+                        arrayAdapter.notifyDataSetChanged();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+
+            @Override
+            public String getBodyContentType() {
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String strUTF8 = null;
+                try {
+                    strUTF8 = new String(response.data, "UTF-8");
+
+                } catch (UnsupportedEncodingException e) {
+
+                    e.printStackTrace();
+                }
+                return Response.success(strUTF8, HttpHeaderParser.parseCacheHeaders(response));
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(stringRequest);
+
     }
 }
